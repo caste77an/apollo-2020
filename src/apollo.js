146b8bd6478cb94149ef93738309bb6abc974c68
castle <castle@castleui-iMac.local>
2021-08-28 02:16:48 +0900

    #2.1 Local State part Two

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,21 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "http://localhost:4000",
   resolvers: {
     Movie: {
       isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+            medium_cover_image: "lalalalal",
+          },
+        });
+      },
     },
   },
 });
